69382b2296d9415038062d9168d87b7a5fa3389f
mackamacka <jamesmckenzie199418@gmail.com>
2025-01-06 17:12:41 +1100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import OutletAnalyzer from './components/OutletAnalyzer';
+import React from 'react'
+import OutletAnalyzer from './components/OutletAnalyzer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4">
+    <div className="min-h-screen bg-gray-100">
       <OutletAnalyzer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
